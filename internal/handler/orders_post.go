cf6c591ea11a45f39794f224915f537483743272
Darya Kabytova <dskabytova@avito.ru>
2025-10-20 02:07:31 +0300

    fix

--- internal/handler/orders_post.go
+++ internal/handler/orders_post.go
@@ -36,31 +36,34 @@ func (h *Handler) PostOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	h.addOrder(ctx, w, userID, orderNum)
+	isDone := h.addOrder(ctx, w, userID, orderNum)
+	if isDone {
+		return
+	}
 
 	w.WriteHeader(http.StatusAccepted)
 }
 
-func (h *Handler) addOrder(ctx context.Context, w http.ResponseWriter, userID int64, orderNumber string) {
+func (h *Handler) addOrder(ctx context.Context, w http.ResponseWriter, userID int64, orderNumber string) bool {
 	if !service.ValidateOrder(orderNumber) {
 		http.Error(w, "invalid order number", http.StatusUnprocessableEntity)
-		return
+		return true
 	}
 
 	orderUserID, err := h.store.GetOrderUserByNumber(ctx, orderNumber)
 	if err != nil && !errors.Is(err, repErr.ErrOrderNotExsit) {
 		h.logger.Errorw("failed to check order", "error", err)
 		http.Error(w, "internal server error", http.StatusInternalServerError)
-		return
+		return true
 	}
 
 	if orderUserID != 0 {
 		if orderUserID == userID {
 			w.WriteHeader(http.StatusOK)
-			return
+			return true
 		} else {
 			http.Error(w, "order already uploaded by another user", http.StatusConflict)
-			return
+			return true
 		}
 	}
 
@@ -68,6 +71,8 @@ func (h *Handler) addOrder(ctx context.Context, w http.ResponseWriter, userID in
 	if err != nil {
 		h.logger.Errorw("failed to add order", "error", err)
 		http.Error(w, "internal server error", http.StatusInternalServerError)
-		return
+		return true
 	}
+
+	return false
 }
