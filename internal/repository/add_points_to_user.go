62741a0a24b5e14f317b9a389435b9fb43db4621
Darya Kabytova <dskabytova@avito.ru>
2025-10-20 01:28:05 +0300

    fix

--- internal/repository/add_points_to_user.go
+++ internal/repository/add_points_to_user.go
@@ -17,14 +17,14 @@ func (r *Repository) AddPointsToUser(ctx context.Context, order dto.Order) error
 		_ = tx.Rollback()
 	}()
 
-	_, err = r.db.ExecContext(ctx, `
+	_, err = tx.ExecContext(ctx, `
 		UPDATE accounts SET balance = balance + $1 WHERE user_id = $2`,
 		entity.Accrual, entity.UserID)
 	if err != nil {
 		return err
 	}
 
-	_, err = r.db.ExecContext(ctx, `
+	_, err = tx.ExecContext(ctx, `
 		UPDATE orders SET status = $1, points = $2 WHERE order_number = $3`,
 		entity.Status, entity.Accrual, entity.Number)
 	if err != nil {
